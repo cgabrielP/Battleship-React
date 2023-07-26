0b87a0d1e94d8b7d9859174c1788f16821c21827
Carlos Peña <cgabriel2303@gmail.com>
2023-07-25 20:59:55 -0400

    add att function

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,31 @@
-import React, {  useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../styles/Board.css'
 // Componente para un cuadro 
-function BoardSquare({ isShip, shipSize, onClick, rowIndex, columnIndex, isRevealed  }) {
-    const shipClass = isShip ? `ship-${shipSize}` : '';
+function BoardSquare({ isShip, shipSize, onClick, rowIndex, columnIndex, isRevealed, isRival }) {
+    const shipClass = (isShip ? `ship-${shipSize}` : '');
     const content = isRevealed ? (isShip ? 'X' : '') : '';
-  
+
+    const handleClick = () => {
+        if (!isRevealed) {
+            onClick(rowIndex, columnIndex);
+        }
+    };
+
     return (
-      <div className={`square ${shipClass}`} onClick={isRevealed ? null : () => onClick(rowIndex, columnIndex)}>
-        {content}
-      </div>
+        <div className={`square ${shipClass}`} onClick={handleClick}>
+            {content}
+        </div>
     );
-  }
+}
 
 
 
 export default function Board() {
     const isFirstRender = useRef(true);
+    const [isPlacingShips, setIsPlacingShips] = useState(true);
     const [selectedShipSize, setSelectedShipSize] = useState(null);
     const [placedShips, setPlacedShips] = useState([]);
     const [isVertical, setIsVertical] = useState(false);
-    const [revealedCells, setRevealedCells] = useState(new Array(10).fill(0).map(() => new Array(10).fill(false)));
     const [boardData, setBoardData] = useState([
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -54,65 +60,66 @@ export default function Board() {
 
     const generateRandomShips = () => {
         const updatedRivalBoard = [...rivalBoardData];
-        let shipSize = 5; 
-      
-        while (shipSize >= 1) {
-          let isValidPosition = false;
-      
-          while (!isValidPosition) {
-            const isVertical = Math.random() < 0.5;
-            const rowIndex = generateRandomPosition();
-            const columnIndex = generateRandomPosition();
-            isValidPosition = true;
-      
-            if (isVertical && rowIndex + shipSize > 10) {
-              isValidPosition = false;
-            } else if (!isVertical && columnIndex + shipSize > 10) {
-              isValidPosition = false;
-            }
-      
-            for (let i = 0; i < shipSize; i++) {
-              const rowIndexToCheck = isVertical ? rowIndex + i : rowIndex;
-              const columnIndexToCheck = isVertical ? columnIndex : columnIndex + i;
-      
-              if (
-                rowIndexToCheck >= 0 &&
-                rowIndexToCheck < 10 &&
-                columnIndexToCheck >= 0 &&
-                columnIndexToCheck < 10 &&
-                updatedRivalBoard[rowIndexToCheck][columnIndexToCheck] !== 0
-              ) {
-                isValidPosition = false;
-                break;
-              }
-            }
-      
-            if (isValidPosition) {
-              for (let i = 0; i < shipSize; i++) {
-                if (isVertical) {
-                  updatedRivalBoard[rowIndex + i][columnIndex] = shipSize;
-                } else {
-                  updatedRivalBoard[rowIndex][columnIndex + i] = shipSize;
-                }
-              }
-            }
-          }
-      
-          shipSize--;
-        }
-      
-        setRivalBoardData(updatedRivalBoard);
-      };
-      
-    
+        let shipSize = 5;
 
-      useEffect(() => {
-        if (isFirstRender.current) {
-          generateRandomShips();
-          isFirstRender.current = false;
+        while (shipSize >= 1) {
+            let isValidPosition = false;
+
+            while (!isValidPosition) {
+                const isVertical = Math.random() < 0.5;
+                const rowIndex = generateRandomPosition();
+                const columnIndex = generateRandomPosition();
+                isValidPosition = true;
+
+                if (isVertical && rowIndex + shipSize > 10) {
+                    isValidPosition = false;
+                } else if (!isVertical && columnIndex + shipSize > 10) {
+                    isValidPosition = false;
+                }
+
+                for (let i = 0; i < shipSize; i++) {
+                    const rowIndexToCheck = isVertical ? rowIndex + i : rowIndex;
+                    const columnIndexToCheck = isVertical ? columnIndex : columnIndex + i;
+
+                    if (
+                        rowIndexToCheck >= 0 &&
+                        rowIndexToCheck < 10 &&
+                        columnIndexToCheck >= 0 &&
+                        columnIndexToCheck < 10 &&
+                        updatedRivalBoard[rowIndexToCheck][columnIndexToCheck] !== 0
+                    ) {
+                        isValidPosition = false;
+                        break;
+                    }
+                }
+
+                if (isValidPosition) {
+                    for (let i = 0; i < shipSize; i++) {
+                        if (isVertical) {
+                            updatedRivalBoard[rowIndex + i][columnIndex] = shipSize;
+                        } else {
+                            updatedRivalBoard[rowIndex][columnIndex + i] = shipSize;
+                        }
+                    }
+                }
+            }
+
+            shipSize--;
         }
-      }, []);
-      
+
+        setRivalBoardData(updatedRivalBoard);
+    };
+
+
+
+
+    useEffect(() => {
+        if (isFirstRender.current) {
+            generateRandomShips();
+            isFirstRender.current = false;
+        }
+    }, []);
+
 
 
     const typeShip = (cell) => {
@@ -131,8 +138,8 @@ export default function Board() {
                 return 0;
         }
     }
-    const chooseBoat = (rowIndex, columnIndex) => {
-        if (selectedShipSize && !placedShips.includes(selectedShipSize)) {
+    const selectBoat = (rowIndex, columnIndex) => {
+        if (isPlacingShips && selectedShipSize && !placedShips.includes(selectedShipSize)) {
             const updatedBoard = [...boardData];
             for (let i = 0; i < selectedShipSize; i++) {
                 // Verifica que no haya un barco en las celdas 
@@ -151,6 +158,11 @@ export default function Board() {
             }
             setBoardData(updatedBoard);
             setPlacedShips([...placedShips, selectedShipSize]);
+            
+            if (placedShips.length === 4) {
+                setIsPlacingShips(false)
+                console.log(isPlacingShips);
+            }
         } else {
             if (placedShips.includes(selectedShipSize)) {
                 alert('Ya has colocado un barco de este tipo. Elige otro tipo de barco.');
@@ -180,7 +192,23 @@ export default function Board() {
     const toggleOrientation = () => {
         setIsVertical((prev) => !prev);
     };
-
+    const attackRival = (rowIndex, columnIndex) => {
+        if (!rivalBoardData[rowIndex][columnIndex].isRevealed) {
+          const updatedRivalBoard = [...rivalBoardData];
+          updatedRivalBoard[rowIndex][columnIndex] = true;
+          setRivalBoardData(updatedRivalBoard);
+    
+          // Aquí puedes verificar si el ataque fue un HIT o un MISS y realizar cualquier acción adicional necesaria
+          const cellValue = rivalBoardData[rowIndex][columnIndex].shipSize;
+          if (cellValue !== 0) {
+            alert('HIT! ¡Has impactado un barco enemigo!');
+          } else {
+            alert('MISS. No has alcanzado ningún barco enemigo.');
+          }
+        } else {
+          alert('Ya has atacado esta celda antes.');
+        }
+      };
     return (
         <>
             <div className='container'>
@@ -192,9 +220,9 @@ export default function Board() {
                                 shipSize={typeShip(cell)}
                                 rowIndex={rowIndex}
                                 columnIndex={columnIndex}
-                                onClick={chooseBoat}
-                                isRevealed={revealedCells[rowIndex][columnIndex]}
-
+                                onClick={selectBoat}
+                                isRevealed={cell.isRevealed}
+                                isRival={false}
                             />
                         ));
                     })}
@@ -208,8 +236,9 @@ export default function Board() {
                                 shipSize={typeShip(cell)}
                                 rowIndex={rowIndex}
                                 columnIndex={columnIndex}
-                                onClick={()=>{console.log('hola')}}
-                                isRevealed={revealedCells[rowIndex][columnIndex]}
+                                isRevealed={false}
+                                isRival={true}
+                                onClick={isPlacingShips ? selectBoat : attackRival}
                             />
                         ));
                     })}
@@ -220,9 +249,9 @@ export default function Board() {
                     <h3>Selecciona el tamaño del barco:</h3>
                     <button className='ship-1' onClick={() => setSelectedShipSize(1)}>Tamaño 1</button>
                     <button className='ship-2' onClick={() => setSelectedShipSize(2)}>Tamaño 2</button>
-                    <button className='ship-3'onClick={() => setSelectedShipSize(3)}>Tamaño 3</button>
-                    <button className='ship-4'onClick={() => setSelectedShipSize(4)}>Tamaño 4</button>
-                    <button className='ship-5'onClick={() => setSelectedShipSize(5)}>Tamaño 5</button>
+                    <button className='ship-3' onClick={() => setSelectedShipSize(3)}>Tamaño 3</button>
+                    <button className='ship-4' onClick={() => setSelectedShipSize(4)}>Tamaño 4</button>
+                    <button className='ship-5' onClick={() => setSelectedShipSize(5)}>Tamaño 5</button>
                 </div>
                 <div className='orientation-selector'>
                     <h3>Orientación del barco:</h3>
@@ -230,7 +259,7 @@ export default function Board() {
                 </div>
 
             </div>
-            <button className='reset'onClick={resetTable}>🔃</button>
+            <button className='reset' onClick={resetTable}>🔃</button>
         </>
     );
 }
